36159e34e19ee863cec49e1318e9f4c877a97cff
Izaque Rodrigues <izaquesjunior@gmail.com>
2021-12-22 20:45:59 -0300

    build

---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PageLogin = () => {
+    return (
+        <div>
+            login
+        </div>
+    )
+}
+
+export default PageLogin
--- src/routes.jsx
+++ src/routes.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import InitialPage from './pages/InitialPage'
+import PageLogin from './pages/Login';
 
 const Rotas = () => (
   <BrowserRouter>
     <Routes>
         <Route path='/barber-manager' element={<InitialPage/>} />  
+        <Route path='/barber-manager/login' element={<PageLogin/>} />  
     </Routes>
   </BrowserRouter>
 );

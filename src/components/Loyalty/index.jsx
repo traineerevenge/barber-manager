bbedac1af3905455371904f40c5ea1649fe08aae
Izaque Rodrigues <izaquesjunior@gmail.com>
2021-12-23 21:09:29 -0300

    pagina de usuario

---
+++ src/components/Loyalty/index.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import './global.css'
+import Star from '../../images/icon--star.png'
+
+const Loyalty = () => {
+    return (
+        <>
+            <div className="loyalty__title">
+                <h2>
+                    # LEALDADE
+                </h2>
+               
+                <p>
+                    Acumule 10 estrelas e troque por um prêmio!
+                </p>
+            </div>
+
+            <section className="loyalty__container">
+               
+                <div className="loyalty__card">
+                    <img src={Star} alt="" />
+                </div>
+
+                <div className="loyalty__card">
+                    <img src={Star} alt="" />
+                </div>
+
+                <div className="loyalty__card">
+                    <img src={Star} alt="" />
+                </div>
+
+                <div className="loyalty__card">
+                    <img src={Star} alt="" />
+                </div>
+
+                <div className="loyalty__card">
+                    <img src={Star} alt="" />
+                </div>
+                <div className="loyalty__card">
+                    <img src={Star} alt="" />
+                </div>
+                <div className="loyalty__card">
+                    <img src={Star} alt="" />
+                </div>
+
+            </section>
+
+            <div className="loyalty__missing-stars">
+                <h3> Faltam 3 estrelas </h3>
+            </div>
+        </>
+    )
+}
+
+export default Loyalty
--- src/pages/UserPage/index.jsx
+++ src/pages/UserPage/index.jsx
@@ -1,11 +1,35 @@
 import React from "react";
 import Header from "../../components/Header";
 import './global.css'
+import BarberBanner from '../../images/barber_banner.jpg'
+import Loyalty from "../../components/Loyalty";
 
 const UserPage = () => {
     return (
         <>
             < Header />
+            <main className="user__main">
+                <section className="user__container">
+                    <div className="user__barber-banner">
+                        <img src={BarberBanner} alt="" />
+                    </div>
+                    <div className="user__barber-description">
+                        <h2>
+                            Hairline - Barber Shop
+                        </h2>
+                        <p>
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                            when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+                            It has survived not only five centuries, but also the leap into electronic typesetting, 
+                            remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset 
+                            sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like 
+                            Aldus PageMaker including versions of Lorem Ipsum.
+                        </p>
+                    </div>
+                </section>
+                <Loyalty />
+            </main>
          </>
     )
 }
